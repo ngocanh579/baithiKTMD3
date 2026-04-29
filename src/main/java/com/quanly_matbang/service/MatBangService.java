18ad345990717e9a67b3fb7cc1efa4a1bd477b11
CJ1024-lethingocanh <ngoc.ltnanh@gmail.com>
2026-04-29 23:22:28 +0700

    update lai file

--- src/main/java/com/quanly_matbang/service/MatBangService.java
+++ src/main/java/com/quanly_matbang/service/MatBangService.java
@@ -5,6 +5,8 @@ import com.quanly_matbang.model.MatBang;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class MatBangService {
@@ -12,30 +14,51 @@ public class MatBangService {
 
     public void add(MatBang matbang) throws Exception {
         if (dao.isExist(matbang.getMaMatBang())) {
-            throw new Exception("Mã đã tồn tại");
+            throw new Exception("Mã mặt bằng đã tồn tại");
         }
+
         java.sql.Date ngayBatDau = matbang.getNgayBatDau();
         java.sql.Date ngayKetThuc = matbang.getNgayKetThuc();
 
         if (ngayBatDau == null || ngayKetThuc == null) {
-            throw new Exception("Ngày không được để trống");
+            throw new Exception("Ngày bắt đầu và ngày kết thúc không được để trống");
         }
 
         LocalDate start = ngayBatDau.toLocalDate();
         LocalDate end = ngayKetThuc.toLocalDate();
 
-        System.out.println("Service dates: " + start + " → " + end);
-
         if (end.isBefore(start.plusMonths(6))) {
-            throw new Exception("Ngày kết thúc phải sau ngày bắt đầu ít nhất 6 tháng");
+            throw new Exception("Thời hạn hợp đồng tối thiểu 6 tháng");
         }
 
-        System.out.println("== BEFORE DAO ==");
         dao.insert(matbang);
-        System.out.println("== AFTER DAO ==");
     }
     public List<MatBang> search(String loai, Double gia, Integer tang) throws Exception {
-        return dao.search(loai, gia, tang);
+        List<MatBang> all = getAll();
+        List<MatBang> result = new ArrayList<>();
+
+        for (MatBang matbang : all) {
+            boolean check = true;
+
+            if (loai != null && !loai.trim().isEmpty()) {
+                check &= matbang.getLoaiMatBang().equals(loai);
+            }
+
+            if (gia != null) {
+                check &= matbang.getGiaTien() == gia;
+            }
+
+            if (tang != null) {
+                check &= matbang.getTang() == tang;
+            }
+
+            if (check) {
+                result.add(matbang);
+            }
+        }
+
+        result.sort(Comparator.comparingDouble(MatBang::getDienTich));
+        return result;
     }
 
     public void delete(String ma) throws Exception {
